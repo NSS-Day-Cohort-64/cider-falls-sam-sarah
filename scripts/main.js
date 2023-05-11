fb4092d5d2af914b44065cc3ec9eafc7f7aca1d0
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-11 09:59:28 -0500

    new Locations module, new main.js, new details.css

--- scripts/main.js
+++ scripts/main.js
@@ -7,21 +7,50 @@ const mainContainer = document.querySelector("#container")
 const applicationHTML = `
 <h1>Cider Falls Park</h1>
 
-<article class="services">
-    <h2>Services</h2>
-    ${Services()}
-</article>
+<div class="services-box">
+  <h2>Services</h2>
+  ${Services()}
+</div>
 
-<article class="details">
+<div class="container">
+  <div class="large-box">
+    <div class="small-box">
+      <img src="thumbnail1.jpg" alt="Thumbnail 1">
+      <p>Box 1</p>
+    </div>
+    <div class="small-box">
+      <img src="thumbnail2.jpg" alt="Thumbnail 2">
+      <p>Box 2</p>
+    </div>
+    <div class="small-box">
+      <img src="thumbnail3.jpg" alt="Thumbnail 3">
+      <p>Box 3</p>
+    </div>
+    <div class="small-box">
+      <img src="thumbnail4.jpg" alt="Thumbnail 4">
+      <p>Box 4</p>
+    </div>
+    <div class="small-box">
+      <img src="thumbnail5.jpg" alt="Thumbnail 5">
+      <p>Box 5</p>
+    </div>
+    <div class="small-box">
+      <img src="thumbnail6.jpg" alt="Thumbnail 6">
+      <p>Box 6</p>
+    </div>
+  </div>
+  
+  <article class="details">
     <section class="guests-list">
-        <h2>Guests</h2>
-        ${GuestNames()}
+      <h2>Guests</h2>
+      ${GuestNames()}
     </section>
     <section class="locations-list">
-        <h2>Park Locations</h2>
-        ${Locations()}
+      <h2>Park Locations</h2>
+      ${Locations()}
     </section>
-</article>
+  </article>
+</div>
 `
 
 mainContainer.innerHTML = applicationHTML
